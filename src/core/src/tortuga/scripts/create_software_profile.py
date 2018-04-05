4a95209225db565a10785834f51f1b5ea487c8c4
Mike Frisch <mfrisch@univa.com>
2018-04-04 20:29:18 -0400

    Remove XML support from create-software-profile; use JSON templates

--- src/core/src/tortuga/scripts/create_software_profile.py
+++ src/core/src/tortuga/scripts/create_software_profile.py
@@ -18,6 +18,7 @@
 
 import glob
 import json
+import argparse
 
 import os.path
 from jinja2 import Template
@@ -32,48 +33,35 @@ from tortuga.wsapi.softwareProfileWsApi import SoftwareProfileWsApi
 
 
 class CreateSoftwareProfileCli(TortugaCli):
-    def __init__(self):
-        super().__init__()
-
-        self._default_tmpl_dir = os.path.join(
-            self._cm.getRoot(), 'share/templates/software')
-
-        option_group_name = _('Information')
-        self.addOptionGroup(option_group_name, '')
-        self.addOptionToGroup(option_group_name, '--list-templates',
-                              action='store_true',
-                              dest='bDisplayTemplateList',
-                              default=False,
-                              help=_('List available software profile'
-                                     ' templates'))
-
+    def parseArgs(self, usage=None):
         # Add options group and options specific to creation of a software
         # profile
         option_group_name = _('Create Software Profile Options')
         option_group = self.addOptionGroup(option_group_name, '')
 
-        excl_option_group = option_group.add_mutually_exclusive_group()
+        self.addOptionToGroup(option_group_name,
+                              '-j', '--json-file', dest='jsonTemplatePath',
+                              help=argparse.SUPPRESS)
 
-        excl_option_group.add_argument(
-            '-x', '--xml-file', dest='templatePath',
-            help=_('Path to hardware profile creation template'))
-
-        excl_option_group.add_argument(
-            '-j', '--json-file', dest='jsonTemplatePath',
-            help=_('Path to JSON-formatted hardware profile creation'
-                   ' template'))
+        self.addOptionToGroup(option_group_name,
+                              '--template', dest='jsonTemplatePath',
+                              help=_('Path to JSON-formatted hardware profile'
+                                     ' creation template'))
 
         self.addOptionToGroup(option_group_name, '--name',
                               action=ParseProfileTemplateArgsAction,
                               help=_('Software profile name'))
+
         self.addOptionToGroup(option_group_name, '--description',
                               action=ParseProfileTemplateArgsAction,
                               dest='description',
                               help=_('Description for software profile'))
+
         self.addOptionToGroup(option_group_name, '--type',
                               action=ParseProfileTemplateArgsAction,
                               dest='profileType',
                               help=_('Software profile type'))
+
         self.addOptionToGroup(option_group_name, '--os',
                               action=ParseOperatingSystemArgAction,
                               metavar='OS SPEC',
@@ -99,13 +87,7 @@ class CreateSoftwareProfileCli(TortugaCli):
             dest='unmanaged',
             help=_('Create an unmanaged software profile'))
 
-    def displayTemplateList(self):
-        filespec = os.path.join(self._default_tmpl_dir, '*.xml')
-
-        templateFiles = glob.glob(filespec)
-
-        if templateFiles:
-            print('\n'.join(templateFiles))
+        super().parseArgs(usage=usage)
 
     def runCommand(self):
         self.parseArgs(_("""
@@ -117,26 +99,37 @@ the template can be overridden by providing the appropriate
 command line options.
 """))
 
-        if self.getArgs().bDisplayTemplateList:
-            self.displayTemplateList()
-            return
+        if self.getArgs().jsonTemplatePath:
+            # load from template
+            if self.getArgs().jsonTemplatePath and \
+                    not os.path.exists(self.getArgs().jsonTemplatePath):
+                raise InvalidCliRequest(
+                    _('Cannot read template from %s') % (
+                        self.getArgs().jsonTemplatePath))
 
-        template_path = self.getArgs().templatePath \
-            if self.getArgs().templatePath else \
-            self.getArgs().jsonTemplatePath
+            try:
+                with open(self.getArgs().jsonTemplatePath) as fp:
+                    tmpl_dict = json.load(fp)
+            except Exception as exc:
+                raise InvalidProfileCreationTemplate(
+                    'Invalid profile creation template: {}'.format(exc))
+        else:
+            tmpl_dict = {}
 
-        b_use_default_template = False
+        if hasattr(self.getArgs(), 'tmplDict'):
+            tmpl_dict = {**tmpl_dict, **getattr(self.getArgs(), 'tmplDict')}
 
-        if not template_path:
-            # Attempt to use default template
-            template_path = os.path.join(
-                self._default_tmpl_dir, 'defaultSoftwareProfile.tmpl.xml')
+        if hasattr(self.getArgs(), 'osInfo'):
+            tmpl_dict['os'] = {
+                'name': getattr(self.getArgs(), 'osInfo').getName(),
+                'version': getattr(self.getArgs(), 'osInfo').getVersion(),
+                'arch': getattr(self.getArgs(), 'osInfo').getArch(),
+            }
 
-            b_use_default_template = True
+        if 'type' not in tmpl_dict:
+            tmpl_dict['type'] = 'compute'
 
-        if not os.path.exists(template_path):
-            raise InvalidCliRequest(
-                _('Cannot read template from %s') % (template_path))
+        sw_profile_spec = SoftwareProfile.getFromDict(tmpl_dict)
 
         api = SoftwareProfileWsApi(username=self.getUsername(),
                                    password=self.getPassword(),
@@ -148,49 +141,12 @@ command line options.
             'unmanagedProfile': self.getArgs().unmanaged,
         }
 
-        try:
-            with open(template_path) as fp:
-                temp_ = fp.read()
-
-            # Process the XML template
-            tmpl = Template(temp_).render(self.getArgs().tmplDict)
-        except Exception as ex:
-            self.getLogger().debug('Error applying template substitutions')
-
-            self.getLogger().exception(ex)
-
-            raise InvalidProfileCreationTemplate(
-                'Invalid profile creation template: %s' % (ex))
-
-        try:
-            if b_use_default_template or self.getArgs().templatePath:
-                # We want to ignore all tags with id and software profile
-                # id...they would be there if the template was created from
-                # a dump of an existing profile
-
-                sw_profile_spec = SoftwareProfile.getFromXml(
-                    tmpl, ['id', 'softwareProfileId'])
-            else:
-                sw_profile_dict = json.loads(tmpl)
-
-                sw_profile_spec = SoftwareProfile.getFromDict(
-                    sw_profile_dict['softwareProfile'])
-        except Exception as ex:
-            self.getLogger().debug('Error parsing XML template')
-            self.getLogger().exception(ex)
-            raise InvalidProfileCreationTemplate(
-                'Invalid profile creation template')
-
-        if not sw_profile_spec:
-            raise InvalidProfileCreationTemplate(
-                'Invalid software creation template')
-
-        if hasattr(self.getArgs(), 'osInfo'):
-            sw_profile_spec.setOsInfo(self.getArgs().osInfo)
-
-        api.createSoftwareProfile(
-            sw_profile_spec, settings_dict)
+        api.createSoftwareProfile(sw_profile_spec, settings_dict)
 
 
 def main():
     CreateSoftwareProfileCli().run()
+
+
+if __name__ == '__main__':
+    main()
