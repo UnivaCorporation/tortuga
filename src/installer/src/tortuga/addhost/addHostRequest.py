67e401d51b7aeb98dfc0f10134ecea24b8f39c4d
Mike Frisch <mfrisch@univa.com>
2018-04-09 16:48:47 -0400

    Move AddHostContextManager to separate module

--- src/installer/src/tortuga/addhost/addHostRequest.py
+++ src/installer/src/tortuga/addhost/addHostRequest.py
@@ -17,31 +17,17 @@
 import datetime
 import json
 import logging
+
 from tortuga.db.dbManager import DbManager
 from tortuga.db.nodeRequestsDbHandler import NodeRequestsDbHandler
-from tortuga.addhost.addHostManager import AddHostManager
 
+from .contextManager import AddHostSessionContextManager
 
-class AddHostSessionContextManager(object):
-    def __init__(self, addHostSession):
-        self.addHostSession = addHostSession
-        self.ahm = None
-
-    def __enter__(self):
-        self.ahm = AddHostManager()
-
-        self.ahm.update_session(self.addHostSession, running=True)
-
-        return self.ahm
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.ahm.update_session(self.addHostSession, running=False)
+logger = logging.getLogger('tortuga.addhost')
+logger.addHandler(logging.NullHandler())
 
 
 def process_addhost_request(addHostSession):
-    logger = logging.getLogger('tortuga.addhost')
-    logger.addHandler(logging.NullHandler())
-
     with DbManager().session() as session:
         req = NodeRequestsDbHandler().get_by_addHostSession(
             session, addHostSession)
@@ -67,10 +53,11 @@ def process_addhost_request(addHostSession):
                 session.delete(req)
 
                 logger.debug(
-                    'process_addhost_request() Add host request [%s] processed'
-                    ' successfully' % (req.addHostSession))
+                    'Add host request [%s] processed successfully' % (
+                        req.addHostSession))
             except Exception as exc:
-                logger.exception('A fatal error occurred during add host workflow')
+                logger.exception(
+                    'Fatal error occurred during add host workflow')
 
                 req.state = 'error'
                 req.message = str(exc)
