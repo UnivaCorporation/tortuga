3dc20e4e50cabe6df2a940fb50f749ab52980d84
Mike Frisch <mfrisch@univa.com>
2018-04-20 11:26:59 -0400

    Added Alembic script for dropping packages related tables

---
+++ src/installer/alembic/versions/c6f886ccdd73_remove_packages_table.py
@@ -0,0 +1,25 @@
+"""remove packages table
+
+Revision ID: c6f886ccdd73
+Revises: d3c8cc11f603
+Create Date: 2018-04-20 11:24:08.990606
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c6f886ccdd73'
+down_revision = 'd3c8cc11f603'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_table('software_profile_packages')
+    op.drop_table('packages')
+
+
+def downgrade():
+    pass
