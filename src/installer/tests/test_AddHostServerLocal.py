40c1ba669e2885049fa9f74eecd738e46119a8cd
Mike Frisch <mfrisch@univa.com>
2018-05-26 10:17:19 -0400

    Futher implementation of AddHostServerLocal unit tests

--- src/installer/tests/test_AddHostServerLocal.py
+++ src/installer/tests/test_AddHostServerLocal.py
@@ -12,10 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=protected-access
+
+import pytest
+
 from tortuga.addhost.addHostServerLocal import (AddHostServerLocal,
                                                 get_host_name)
+from tortuga.db.hardwareProfilesDbHandler import HardwareProfilesDbHandler
 from tortuga.db.models.node import Node
 from tortuga.db.networksDbHandler import NetworksDbHandler
+from tortuga.db.softwareProfilesDbHandler import SoftwareProfilesDbHandler
+from tortuga.exceptions.networkNotFound import NetworkNotFound
+from tortuga.exceptions.invalidMacAddress import InvalidMacAddress
+
 
 api = AddHostServerLocal()
 
@@ -48,6 +57,70 @@ def test_generate_provisioning_ip_address(dbm):
         result = api.generate_provisioning_ip_address(networks[0])
 
         assert result
+
+def test_failed_initializeNode(dbm):
+    with dbm.session() as session:
+        hardware_profile = HardwareProfilesDbHandler().getHardwareProfile(
+            session, 'nonetwork'
+        )
+
+        software_profile = SoftwareProfilesDbHandler().getSoftwareProfile(
+            session, 'compute'
+        )
+
+        node = Node()
+
+        with pytest.raises(NetworkNotFound):
+            api.initializeNode(
+                session, node, hardware_profile, software_profile, [])
+
+
+def test_initializeNode(dbm):
+    with dbm.session() as session:
+        hardware_profile = HardwareProfilesDbHandler().getHardwareProfile(
+            session, 'localiron'
+        )
+
+        software_profile = SoftwareProfilesDbHandler().getSoftwareProfile(
+            session, 'compute'
+        )
+
+        node = Node()
+
+        nic_defs = [
+            {
+                'mac': '00:00:00:00:00:01',
+            }
+        ]
+
+        api.initializeNode(
+            session, node, hardware_profile, software_profile, nic_defs)
+
+        assert node.name
+        assert node.nics and len(node.nics) == 1 and \
+            node.nics[0].mac == '00:00:00:00:00:01' and \
+            node.nics[0].boot
+
+
+@pytest.mark.parametrize('mac', [
+    '000000000000',
+    '00:00:00:00:00:01',
+    'abababababab',
+    pytest.param('ab', marks=pytest.mark.xfail(raises=InvalidMacAddress)),
+    pytest.param(None, marks=pytest.mark.xfail(raises=InvalidMacAddress)),
+    pytest.param('', marks=pytest.mark.xfail(raises=InvalidMacAddress)),
+    pytest.param('xxxxxxxxxxxx',
+                 marks=pytest.mark.xfail(raises=InvalidMacAddress)),
+    pytest.param('ababababababaa',
+                 marks=pytest.mark.xfail(raises=InvalidMacAddress)),
+    pytest.param('ab:ax:bc:ed:ee:aa',
+                 marks=pytest.mark.xfail(raises=InvalidMacAddress)),
+])
+def test_validate_mac_address(dbm, mac):
+    with dbm.session() as session:
+        prov_network = NetworksDbHandler().getNetworkList(session)[0]
+
+        api._validate_mac_address(session, mac, prov_network)
 
 
 def test_get_host_name():
