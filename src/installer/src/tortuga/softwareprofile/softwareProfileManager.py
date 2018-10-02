b3f429b586d2c8c64592d74bd45184c142d1208b
dtougas <dtougas@univa.com>
2018-10-02 14:09:24 -0400

    Catch errors when enabling components and exit more gracefully

--- src/installer/src/tortuga/softwareprofile/softwareProfileManager.py
+++ src/installer/src/tortuga/softwareprofile/softwareProfileManager.py
@@ -490,16 +490,24 @@ class SoftwareProfileManager(TortugaObjectManager): \
         software_profile = self.getSoftwareProfile(
             session, software_profile_name, {'os': True})
 
-        if kit.getIsOs():
-            best_match_component = self._enable_os_kit_component(
-                session, kit, comp_name, comp_version, software_profile)
-        else:
-            best_match_component = self._enable_kit_component(
-                session, kit, comp_name, comp_version, software_profile)
+        try:
+            if kit.getIsOs():
+                best_match_component = self._enable_os_kit_component(
+                    session, kit, comp_name, comp_version, software_profile)
+
+            else:
+                best_match_component = self._enable_kit_component(
+                    session, kit, comp_name, comp_version, software_profile)
+
+        except Exception as ex:
+            self.getLogger().error(
+                "Error encountered while enabling component: {}".format(ex))
+            return
 
         if not best_match_component:
             self.getLogger().info(
                 'Component not enabled: {}'.format(comp_name))
+
         else:
             self.getLogger().info(
                 'Enabled component on software profile: {} -> {}'.format(
