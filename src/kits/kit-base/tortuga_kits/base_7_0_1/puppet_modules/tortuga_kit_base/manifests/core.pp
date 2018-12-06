0d04e9f318e1d42a131e6044918c722bb6ee9558
dtougas <dtougas@univa.com>
2018-12-06 11:48:32 -0500

    Merge fixes from Unicloud

--- src/kits/kit-base/tortuga_kits/base_7_0_1/puppet_modules/tortuga_kit_base/manifests/core.pp
+++ src/kits/kit-base/tortuga_kits/base_7_0_1/puppet_modules/tortuga_kit_base/manifests/core.pp
@@ -13,53 +13,32 @@
 # limitations under the License.
 
 
-class tortuga_kit_base::core (
-  Variant[String, Undef] $proxy_uri = undef,
-  Variant[String, Undef] $proxy_user = undef,
-  Variant[String, Undef] $proxy_password = undef,
-  Variant[String, Undef] $puppet_proxy_http_host = undef,
-  Variant[Integer, Undef] $puppet_proxy_http_port = undef,
-  Variant[String, Undef] $puppet_proxy_http_user = undef,
-  Variant[String, Undef] $puppet_proxy_http_password = undef,
-) {
-  contain tortuga_kit_base::core::config
-  contain tortuga_kit_base::core::actions
-  contain tortuga_kit_base::core::done
-}
-
-class tortuga_kit_base::core::config {
-  class { 'tortuga::config':
-    proxy_uri                  => $tortuga_kit_base::core::proxy_uri,
-    proxy_user                 => $tortuga_kit_base::core::proxy_user,
-    proxy_password             => $tortuga_kit_base::core::proxy_password,
-    puppet_proxy_http_host     => $tortuga_kit_base::core::puppet_proxy_http_host,
-    puppet_proxy_http_port     => $tortuga_kit_base::core::puppet_proxy_http_port,
-    puppet_proxy_http_user     => $tortuga_kit_base::core::puppet_proxy_http_user,
-    puppet_proxy_http_password => $tortuga_kit_base::core::puppet_proxy_http_password,
-  }
-  contain tortuga::config
-
-  class { 'tortuga::packages':
-    require => Class['tortuga::config'],
-  }
-
-  contain tortuga::packages
-}
-
-class tortuga_kit_base::core::actions {
+class tortuga_kit_base::core {
   # Incorporate classes defined in Hiera
   $classes = lookup('classes', {'merge' => 'unique', 'default_value' => []})
   if $classes {
     include($classes)
   }
 
+  contain tortuga_kit_base::core::packages
+  contain tortuga_kit_base::core::worker
+  contain tortuga_kit_base::core::installed
+}
+
+class tortuga_kit_base::core::packages {
+  contain tortuga::packages
+}
+
+class tortuga_kit_base::core::worker {
+  require tortuga_kit_base::core::packages
+
   contain tortuga::compute
   contain tortuga_kit_base::core::cfmsecret
+  contain tortuga_kit_base::core::ntpd
   contain tortuga_kit_base::core::ssh_server
   contain tortuga_kit_base::core::links
   contain tortuga_kit_base::common::nfs
   contain tortuga_kit_base::core::install
-  contain tortuga_kit_base::core::certificate_authority
   contain tortuga::envscript
 
   contain tortuga_kit_base::core::post_install
@@ -69,8 +48,8 @@ class tortuga_kit_base::core::actions {
     -> Class['tortuga_kit_base::core::links']
 }
 
-class tortuga_kit_base::core::done {
-  require tortuga_kit_base::core::actions
+class tortuga_kit_base::core::installed {
+  require tortuga_kit_base::core::worker
 
   tortuga_kit_base::installed { 'compute': }
 }
