536d6eb1a5057f1cfb82709414e715db0349817c
dtougas <dtougas@univa.com>
2019-08-16 15:00:26 -0400

    More puppet fixes
    (cherry picked from commit 440cb97c8e80ec902ffc1e923d09db35a4d9aedd)

--- src/puppet/univa-tortuga/manifests/installer/apache.pp
+++ src/puppet/univa-tortuga/manifests/installer/apache.pp
@@ -26,11 +26,8 @@ class tortuga::installer::apache::package {
 class tortuga::installer::apache::config {
   require tortuga::installer::apache::package
 
-  # Ensure SSLv3 is disabled by default (req'd for POODLE exploit)
-  augeas { 'disable_ssl_v3':
-    context => '/files/etc/httpd/conf.d/ssl.conf',
-    changes => 'set VirtualHost/directive[.="SSLProtocol"]/arg[last()+1] -SSLv3',
-    onlyif  => 'match VirtualHost/directive[.="SSLProtocol"][arg="-SSLv3"] size == 0',
+  file { '/etc/httpd/conf.d/ssl.conf':
+    ensure => absent
   }
 
   augeas { 'stop_listening_80':
