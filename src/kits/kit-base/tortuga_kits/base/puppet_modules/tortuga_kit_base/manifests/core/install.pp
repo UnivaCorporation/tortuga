a24565de2851059d9f512d4f1588c69ba1abdd04
Stanislav Tereshchenko <stereshchenko@univa.com>
2019-10-15 09:23:22 -0400

    added dependency

--- src/kits/kit-base/tortuga_kits/base/puppet_modules/tortuga_kit_base/manifests/core/install.pp
+++ src/kits/kit-base/tortuga_kits/base/puppet_modules/tortuga_kit_base/manifests/core/install.pp
@@ -164,6 +164,7 @@ class tortuga_kit_base::core::install::install_tortuga_python_package(
   String $package)
 {
   include tortuga::config
+  require tortuga_kit_base::core::install::create_tortuga_instroot
 
   $intweburl = "http://${::primary_installer_hostname}:${tortuga::config::int_web_port}"
 
