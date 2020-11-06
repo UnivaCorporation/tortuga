95d8a8de0b10c89ce12477d5c75cfe7739370d95
Tanner Prestegard <tprestegard@altair.com>
2020-11-06 10:44:37 -0600

    offline install: remove ActiveMQ and MCollective

--- src/scripts/prep-offline-install.sh
+++ src/scripts/prep-offline-install.sh
@@ -56,7 +56,6 @@ distver=$(rpm --query centos-release --queryformat "%{VERSION}")
 # master package list
 readonly pkgs="puppet-agent \
 puppetserver \
-activemq \
 git \
 rh-python36 \
 redis \
@@ -301,13 +300,6 @@ download_rpms ${required_pkgs}
 download_puppet_modules ${puppet_module_urls}
 
 download_python_packages
-
-# other packages
-mkdir -p ${dstdir}/other/3rdparty/mcollective-puppet-agent
-( cd ${dstdir}/other/3rdparty/mcollective-puppet-agent; curl -s -LO https://github.com/puppetlabs/mcollective-puppet-agent/archive/1.13.1.tar.gz )
-
-mkdir -p ${dstdir}/other/3rdparty/mcollective-shell-agent
-( cd ${dstdir}/other/3rdparty/mcollective-shell-agent; curl -s -LO https://github.com/choria-legacy/mcollective-shell-agent/archive/0.0.2.tar.gz )
 
 echo
 echo "Dependencies download complete."
